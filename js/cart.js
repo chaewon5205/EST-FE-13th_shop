335783f0021c60f51f8f120df67e95deda0cd4da
chaewon5205 <parkjihae9262@gmail.com>
2026-06-08 21:00:36 +0900

    feat: 장바구니 페이지 상품 목록 출력

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,41 @@
 import { readCart, updateCartCount } from "./utils/common.js";
 
 updateCartCount();
+
 const cart = readCart();
-console.log(cart);
+
+const cartList = document.querySelector(".cart-list");
+
+const fragment = document.createDocumentFragment();
+
+cart.forEach(item => {
+  const article = document.createElement("article");
+
+  article.className = "cart-item";
+
+  article.innerHTML = `
+    <span class="item-check">
+      <span class="check-box"></span>
+    </span>
+
+    <div class="cart-thumb">
+      <img src="${item.thumb}" alt="${item.title}">
+    </div>
+
+    <div class="cart-item-info">
+      <h2>${item.title}</h2>
+      <p>${item.brand}</p>
+      <strong>수량 : ${item.qty}</strong>
+    </div>
+
+    <div class="quantity-box">
+      <button type="button">-</button>
+      <span>${item.qty}</span>
+      <button type="button">+</button>
+    </div>
+  `;
+
+  fragment.append(article);
+});
+
+cartList.append(fragment);
